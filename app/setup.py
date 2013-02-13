4959cc9461c439da5dbfdbdf7d4aeb6c0338e36c
Oliver Tonnhofer <olt@bogosoft.com>
2013-02-13 11:59:14 +0100

    update setup.py

--- app/setup.py
+++ app/setup.py
@@ -13,15 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# -*- coding: utf-8 -*-
 """
-    setup.py
-    ~~~~~~~~
-
-    :copyright: (c) 2012 by Omniscale GmbH & Co. KG.
-"""
-
-""""""""""
 GBI Server
 ----------
 
@@ -30,38 +22,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='gbi_server',
-    version='0.1',
-    url='<enter URL here>',
-    license='BSD',
+    version='1.0',
+    url='https://github.com/omniscale/gbi-server',
+    license='Apache License 2.0',
     author='Oliver Tonnhofer',
     author_email='support@omniscale.de',
-    description='<enter short description here>',
+    description='GBI-Server application',
     long_description=__doc__,
     packages=find_packages(),
     zip_safe=False,
-    platforms='any',
-    install_requires=[
-        'Babel==0.9.6',
-        'cssmin==0.1.4',
-        'Flask-Assets==0.7',
-        'Flask-Babel==0.8',
-        'Flask-Login==0.1.3',
-        'Flask-Mail==0.7.3',
-        'Flask-SQLAlchemy==0.16',
-        'Flask-WTF==0.8',
-        'Flask==0.9',
-        'py-bcrypt==0.2',
-        'PyYAML==3.10',
-        'requests==1.0.4',
-        'SQLAlchemy==0.7.9',
-        'GeoAlchemy==0.7.1',
-        'Werkzeug==0.8.3',
-        'WTForms==1.0.2',
-        'Fiona==0.8',
-        'Shapely==1.2.16',
-        'scriptine==0.2.0a4',
-        'MapProxy==1.5.0',
-    ],
+    install_requires=[], # see requirements.txt
     include_package_data=True,
     package_data={'': ['*.*']},
 )
